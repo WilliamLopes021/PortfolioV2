3d9af1234f6b43ac61496c1e30802f677338db2c
DinoPimenteiro <gabrielwilliam234@gmail.com>
2026-03-30 20:10:04 -0300

    refactor navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,50 +1,78 @@
 import { LuSunMedium, LuSunMoon } from "react-icons/lu";
 import { PiListDashes } from "react-icons/pi";
+import { IoIosClose } from "react-icons/io";
 import Button from "../components/Button";
 import { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/llogodois.png";
 import { navLinks } from "../data/navLinks";
 
 function Navbar() {
-  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const [menuOpen, setMenuOpen] = useState<boolean>(true);
   const [isLight, setIsLight] = useState<boolean>(true);
 
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  const toggleLight = () => {
+    setIsLight((prev) => !prev);
+  };
 
   return (
     <header
-      className={`p-6 bg-slate-300 flex flex-col gap-3 md:flex-row md:justify-between md:items-center`}
+      className={`p-6 w-full bg-slate-300 md:flex flex-col gap-3 md:flex-row md:items-center text-lg`}
     >
-      <nav className="md:flex w-full">
-        <a href="#" className="self-start">
-          <img src={logo} alt="logotipo" className="w-2xs" />
-        </a>
-
-        <ul
-          className={`${menuOpen ? `flex` : `hidden`} md:flex md:flex-row md:items-center md:gap-6 ml-auto mr-30 flex-col w-min`}
-        >
-          {navLinks.map((value) => {
-            return (
-              <li key={value.href} className="">
-                <a href={value.href}> {value.text}</a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-      <div
-        className={`${menuOpen ? `flex` : `hidden`} md:flex md:flex-row md:gap-6 justify-end`}
-      >
-        <button onClick={() => setIsLight(!isLight)} className="cursor-pointer">
-          {isLight ? <LuSunMedium size={30} /> : <LuSunMoon size={30} />}
-        </button>
-        <Button text="Contate-me" />
+      <div className={`md:flex md:gap-10 md:w-full md:justify-between`}>
+        <nav className="md:flex mr-auto ">
+          <a href="#" className="">
+            <img src={logo} alt="logotipo" className="w-2xs" />
+          </a>
+          <ul className={`hidden md:flex md:items-center md:gap-6`}>
+            {navLinks.map((value) => {
+              return (
+                <li key={value.text} className="">
+                  <a href={value.href}> {value.text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <div className="hidden md:flex">
+          <button onClick={() => toggleLight()} className="cursor-pointer">
+            {isLight ? <LuSunMedium size={30} /> : <LuSunMoon size={30} />}
+          </button>
+          <Button text="Contate-me" />
+        </div>
       </div>
+
       <button
-        className="md:hidden top-13 right-20 absolute cursor-pointer"
-        onClick={() => setMenuOpen(!menuOpen)}
+        className="md:hidden top-19 right-20 absolute cursor-pointer"
+        aria-label="abrir-menu"
+        onClick={() => toggleMenu()}
       >
-        <PiListDashes size={30} />
+        {menuOpen ? <IoIosClose size={35} /> : <PiListDashes size={30} />}
       </button>
+      <header className="md:hidden flex flex-col gap-8 text-xl">
+        <nav className={`${menuOpen ? `flex flex-col` : `hidden`} md:hidden`}>
+          <ul className={``}>
+            {navLinks.map((value) => {
+              return (
+                <li key={value.text}>
+                  <a href={value.href}> {value.text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <div
+          className={`${menuOpen ? `flex ` : `hidden`} md:hidden items-center`}
+        >
+          <button onClick={() => toggleLight()} className="cursor-pointer">
+            {isLight ? <LuSunMedium size={30} /> : <LuSunMoon size={30} />}
+          </button>
+          <Button text="Contate-me" />
+        </div>
+      </header>
     </header>
   );
 }
