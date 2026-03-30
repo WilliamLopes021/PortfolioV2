e3e6e931878ad72bec1e0fc1058d7f38991a813b
DinoPimenteiro <gabrielwilliam234@gmail.com>
2026-03-30 20:12:31 -0300

    decorate contact card

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -2,13 +2,13 @@ import type { ContactCardProps } from "../props/ContactCardProps";
 
 function ContactCard({icon, contact, type}: ContactCardProps) {
   return (
-    <div>
-      <div>
+    <div className="p-6 flex border my-3 border-zinc-200 rounded-xl shadow-xl">
+      <div className="bg-sky-100 w-min p-3 rounded-xl">
         {icon}
       </div>
-      <div>
-        <span>{type}</span>
-        <span>{contact}</span>
+      <div className="flex flex-col pl-3 ">
+        <span className="text-gray-400">{type}</span>
+        <span className="font-bold">{contact}</span>
       </div>
     </div>
   );
